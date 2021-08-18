f1a6ab7d47f40902548b351b7aa819a0e1ae7fae
VitaliyKrymov <krymvitaliy@gmail.com>
2021-08-18 22:52:19 +0300

    start

--- file1.js
+++ file1.js
@@ -4,9 +4,12 @@ test();
 console.log(__dirname);// текущий путь к папке
 console.log(__filename);// текущий файл
 // код вище також запускається при запуску файла
+
 module.exports = {
+
     greeting: function (name) {
         console.log('file1.js work')
+
         console.log('Hello ',name)
 }
 }
